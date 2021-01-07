8c05b395f3e3a52ce22d088169033ddda8fed464
MishaJain19 <misha.19.10.1995>
2021-01-07 17:46:38 +0530

    space

--- sketch.js
+++ sketch.js
@@ -130,3 +130,8 @@ function mouseDragged(){
   function mouseReleased(){
     slingShot.fly();
   }
+  function keyPressed(){
+      if(keyCode===32){
+          slingShot.attach(this.ball);
+      }
+  }
